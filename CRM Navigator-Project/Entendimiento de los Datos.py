594c40751647c308e4a8fa44c5fbf860babf1fcc
Victoria Vargas <147217876+victoriavargas28@users.noreply.github.com>
2024-03-20 09:50:22 -0500

    Update Entendimiento de los Datos.py
    Signed-off-by: Victoria Vargas <147217876+victoriavargas28@users.noreply.github.com>

--- CRM Navigator-Project/Entendimiento de los Datos.py
+++ CRM Navigator-Project/Entendimiento de los Datos.py
@@ -1,1 +1,30 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Leer el archivo CSV
+customers = pd.read_csv('Customers.csv')
+custumers.head(5)
+
+# Descripción de Datos
+def data_description(customers, head=5):
+  print("FORMA: ".center(70, '-'))
+  print("Filas: {}".format(customers.shape[0]))
+  print("Columnas: {}".format(costumers.shape[1]))
+  print("TIPOS: ".center(70, '-'))
+  print(customers.dtypes)
+  print("HEAD: ".center(70, '-'))
+  printt(customers.head(head))
+  print("TAIL: ".center(70, '-'))
+  print(costumers.tail(head))
+  print("VALORES NULOS: ".center(70, '-'))
+  print(customers.isnull().sum())
+  print("VALORES DUPLICADOS: ".center(70, '-'))
+  print(customers.duplicated().sum())
+  print("QUIANTILES: ".center(70, '-'))
+  print(customers.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+data_description(customers)
+# Limpieza de datos
+#cust_clean = customers
 
