cdbd4ff326c9f6313369ef572008b4643cfe0e8d
Victoria Vargas <147217876+victoriavargas28@users.noreply.github.com>
2024-03-20 20:26:06 -0500

    Update Entendimiento de los Datos.py
    Signed-off-by: Victoria Vargas <147217876+victoriavargas28@users.noreply.github.com>

--- CRM Navigator-Project/Entendimiento de los Datos.py
+++ CRM Navigator-Project/Entendimiento de los Datos.py
@@ -25,6 +25,38 @@ def data_description(customers, head=5):
   print(customers.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
 
 data_description(customers)
-# Limpieza de datos
-#cust_clean = customers
 
+# Limpieza de datos
+
+cust_clean= customers.dropna()
+print("El total de datos después de remover los vacíos es de:", len(cust_clean))
+
+# Visualización de los datos
+
+  # Gráfico de barras para distribución de género
+gender = cust_clean['Gender'].valuecounts().reset_index()
+gender.columns = ['Gender', 'Count']
+plt.figure(figsize=(8, 6))
+plt.pie(gender, labels=gender.idex, autopct='%1.1f%%', startangle=90, colors['skyblue', 'pink'])
+plt.title('Gender Distribution')
+plt.axis('equal') 
+plt.show()
+
+  # Gráficos de historiograma para valores numéricos
+sns.set(style='whitegrid')
+plt.figure(figsize=(15, 8))
+for i, col in enumerate(['Age', 'Annual Income ($)', 'Spending Score (1-100)', 'Work Experience', 'Family Size'], start=1):
+    plt.subplot(2, 3, i)
+    sns.histplot(data[col], bins=20, kde=True)
+    plt.title(f'Distribution of {col}')
+
+plt.tight_layout()
+plt.show()
+
+  # Gráficos de barras para categorías
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+sns.countplot(x='Gender', cust_clean=cust_clean)
+plt.title('Recuento de Clientes por Profesión')
+plt.tight_layout()
+plt.show()
