25d804b068776f4dd3c921bcaf99e0d19319c153
Jhuomar B. Barria Q <124087234+Jhuomar-Barria@users.noreply.github.com>
2024-03-23 01:46:04 -0500

    Update Recomendaciones y Acciones.py
    Signed-off-by: Jhuomar B. Barria Q.  <124087234+Jhuomar-Barria@users.noreply.github.com>

--- CRM Navigator-Project/Recomendaciones y Acciones.py
+++ CRM Navigator-Project/Recomendaciones y Acciones.py
@@ -14,7 +14,8 @@ df['Gasto Mensual Total'] = df['Work Experience'] * 1000 + df['Family Size'] * 5
 df['Porcentaje de Ahorro'] = ((df['Ingreso Mensual'] - df['Gasto Mensual Total']) / df['Ingreso Mensual']) * 100
 
 # Visualizar los resultados
-print(df[['CustomerID', 'Ingreso Mensual', 'Gasto Mensual Total', 'Porcentaje de Ahorro']])
+print("Tabla de Porcentaje de Ahorro por Cliente:\n")
+display(df[['CustomerID', 'Ingreso Mensual', 'Gasto Mensual Total', 'Porcentaje de Ahorro']])
 
 # Gráfico de barras para visualizar el porcentaje de ahorro
 plt.figure(figsize=(10, 6))
@@ -24,7 +25,4 @@ plt.ylabel('Porcentaje de Ahorro')
 plt.title('Porcentaje de Ahorro por Cliente')
 plt.xticks(df['CustomerID'])
 plt.show()
-
-#El objetivo es ya que cada cliente posee un presupuesto diferente, 
-#hacer un plan de ahorro que sea el mas adecuado y que vaya acorde con su presupuesto mensual
 
